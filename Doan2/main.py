6f7f9d2aff8adda9c570213352561b12de7b4357
kinhbanh123 <tnguyenbinhxyz@gmail.com>
2021-12-17 12:23:55 +0700

    fix

--- Doan2/main.py
+++ Doan2/main.py
@@ -1,4 +1,4 @@
-import pickle
+﻿import pickle
 from typing import List
 from _Class.Data.Canhan import Canhan
 from _Class.Data.Sinhvien import Sinhvien
@@ -13,15 +13,15 @@ from _Class.Data.Lophoc import Lophoc
 
 class ip():
     def InputCanhan(i):
-        inputHoten = input("hay nhap ho ten ")
+        inputHoten = input("hãy nhập họ tên ")
         canhanList[i].hoten = inputHoten
         canhanList[i].macanhan = i
-        canhanList[i].sdt = input("hay nhap sdt ")
-        canhanList[i].quequan = input("hay nhap que quan ")
-        canhanList[i].email = input("hay nhap email ")
+        canhanList[i].sdt = input("hãy nhập sdt ")
+        canhanList[i].quequan = input("hãy nhập quê quán ")
+        canhanList[i].email = input("hãy nhập email ")
         print(canhanList[i].outputInfo())
     def OutputCanhan(i):
-        ff = open('./Doan2_DataB/canhan.txt','a+')
+        ff = open('./Doan2/_DataB/canhan.txt','a+')
         ff.write(canhanList[i].hoten)
         ff.write("\n")
         ff.write(str(canhanList[i].macanhan))
@@ -34,17 +34,17 @@ class ip():
         ff.write("\n")
         ff.close
     def InputSinhvien(i):
-        inputHoten = input("hay nhap ho ten ")
+        inputHoten = input("hãy nhập họ tên ")
         canhanList[i].hoten = inputHoten
         canhanList[i].macanhan = i
-        canhanList[i].sdt = input("hay nhap sdt ")
-        canhanList[i].quequan = input("hay nhap que quan ")
-        canhanList[i].email = input("hay nhap email ")
+        canhanList[i].sdt = input("hãy nhập sdt ")
+        canhanList[i].quequan = input("hãy nhập quê quán ")
+        canhanList[i].email = input("hãy nhập email ")
         print(canhanList[i].outputInfo())
         sinhvienList[i].masinhvien = i
-        sinhvienList[i].ngaysinh = input("hay nhap ngay sinh ") 
-        sinhvienList[i].gioitinh = input("hay nhap gioi tinh ") 
-        sinhvienList[i].diachi = input("hay nhap dia chi ")
+        sinhvienList[i].ngaysinh = input("hãy nhập ngay sinh ") 
+        sinhvienList[i].gioitinh = input("hãy nhập gioi tinh ") 
+        sinhvienList[i].diachi = input("hãy nhập dia chi ")
         print(sinhvienList[i].masinhvien)
     def OutputSinhvien(i):
         ff = open('./Doan2/_DataB/sinhvien.txt','a+')
@@ -68,17 +68,17 @@ class ip():
         ff.write("\n")
         ff.close
     def InputGiaovien(i):
-        inputHoten = input("hay nhap ho ten ")
+        inputHoten = input("hãy nhập họ tên ")
         canhanList[i].hoten = inputHoten
         canhanList[i].macanhan = i
-        canhanList[i].sdt = input("hay nhap sdt ")
-        canhanList[i].quequan = input("hay nhap que quan ")
-        canhanList[i].email = input("hay nhap email ")
+        canhanList[i].sdt = input("hãy nhập sdt ")
+        canhanList[i].quequan = input("hãy nhập quê quán ")
+        canhanList[i].email = input("hãy nhập email ")
         print(canhanList[i].outputInfo())
         giaovienList[i].magiaovien = i
-        giaovienList[i].ngaysinh = input("hay nhap ngay sinh ") 
-        giaovienList[i].gioitinh = input("hay nhap gioi tinh ") 
-        giaovienList[i].diachi = input("hay nhap dia chi ")
+        giaovienList[i].ngaysinh = input("hãy nhập ngay sinh ") 
+        giaovienList[i].gioitinh = input("hãy nhập gioi tinh ") 
+        giaovienList[i].diachi = input("hãy nhập dia chi ")
     def OutputGiaovien(i):
         ff = open('./Doan2/_DataB/giaovien.txt','a+')
         ff.write(canhanList[i].hoten)
@@ -102,18 +102,18 @@ class ip():
         ff.close
     def InputKhoahoc(i):
         khoahocList[i].makhoahoc = i 
-        khoahocList[i].tenkhoahoc = input("hay nhap ten khoa hoc ")
+        khoahocList[i].tenkhoahoc = input("hãy nhập ten khoa hoc ")
 
 
     def InputNganhdaotao(i):
         nganhdaotaoList[i].manganh = i
-        nganhdaotaoList[i].tennganh = input("hay nhap ten nganh ")
+        nganhdaotaoList[i].tennganh = input("hãy nhập ten nganh ")
 
     def InputDiem(i):
-        diemList[i].diemtb = input("hay nhap diem tb ")
-        diemList[i].diemthilan1 = input(" hay nhap diem thi lan thu 1 ")
-        diemList[i].diemthilan2 = input(" hay nhap diem thi lan thu 2 ")
-        diemList[i].hocky = input("hay nhap diem hoc ky ")
+        diemList[i].diemtb = input("hãy nhập diem tb ")
+        diemList[i].diemthilan1 = input(" hãy nhập diem thi lan thu 1 ")
+        diemList[i].diemthilan2 = input(" hãy nhập diem thi lan thu 2 ")
+        diemList[i].hocky = input("hãy nhập diem hoc ky ")
         diemList[i].diemtbc = (int(diemList[i].diemthilan1) + int(diemList[i].diemthilan2) + int(diemList[i].hocky)*3)/5
 
 
@@ -127,14 +127,14 @@ diemList = [Diem() for i in range (0,1000)]
 
 class quanlysv():
     def findsinhvien(self):
-        print("hay nhap ma sinh vien")
+        print("hãy nhập mã sinh viên")
         n = int(input())
         print(canhanList[n].hoten)
     def hienthithongtin(self):
         print(canhanList[i].outputInfo)
         print(sinhvienList[i].outputInfo)
     def xuatthongtin(i):
-        print("hay nhap ten file can xuat ")
+        print("hãy nhập ten file can xuat ")
         tenfile = input()
         f = open(tenfile,'a+')
         f.write(canhanList[i].outputInfo())
@@ -143,14 +143,14 @@ class quanlysv():
 
 class quanlygv():
     def findgiaovien(self):
-        print("hay nhap ma giao vien")
+        print("hãy nhập mã giáo viên")
         n = int(input())
         print(canhanList[n].hoten)
     def hienthithongtin(self):
         print(canhanList[i].outputInfo)
         print(giaovienList[i].outputInfo)
     def xuatthongtin(i):
-        print("hay nhap ten file can xuat ")
+        print("hãy nhập ten file can xuat ")
         tenfile = input()
         f = open(tenfile,'a+')
         f.write(canhanList[i].outputInfo())
@@ -180,13 +180,13 @@ if __name__ == '__main__':
     i = int(ee.readline())
     docdata 
     while tuychon != "close":
-        print("Ban muon thuc hien gi (0 neu muon tat)")
-        print("1 de nhap ten sinh vien ")
-        print("2 de nhap ten giao vien ")
-        print("3 de nhap ten khoa hoc ")
-        print("4 de nhap nganh dao tao ")
-        print("5 de nhap diem ")
-        print("6 de luu du lieu ")
+        print("Bạn muốn thực hiện gì (Nhấn 0 để tắt)")
+        print("1 để nhập tên sinh viên ")
+        print("2 để nhập tên giáo viên ")
+        print("3 để nhập tên khoá học ")
+        print("4 để nhập ngành đào tạo ")
+        print("5 để nhập điểm ")
+        print("6 để lưu dữ liệu ")
         ttcheck = int(input())
         if ttcheck == 0:
             tuychon = "close"
@@ -199,7 +199,7 @@ if __name__ == '__main__':
         if ttcheck == 4:
             ip.InputNganhdaotao(i)
         if ttcheck ==5:
-            print("hay nhap ma sinh vien de nhap diem")
+            print("hãy nhập mã sinh viên để nhập điểm")
             mas = int(input())
             ip.InputDiem(mas)
         if ttcheck ==6:
